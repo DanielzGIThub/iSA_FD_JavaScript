cb59d2e5612bf11924863e37899183d277deeaef
DanielzGIThub <dzmsgbox@gmail.com>
2019-10-08 15:05:22 +0200

    Logical operators tested

--- hello.js
+++ hello.js
@@ -1,18 +1,18 @@
 var myPocketContent = '100';
 
-if (myPocketContent === 100){
+if (myPocketContent === 100 || myPocketContent === '100'){
 
     document.write("You have got in your pocket 100");
 
 } else {
 
-    document.write("You have got in your pocket '100'");
+    document.write("Check your pocket");
 
 }
 
-if (myPocketContent !== '100'){
+if (myPocketContent !== 100 && myPocketContent !== 1000){
 
-    document.write("You have got in your pocket 100");
+    document.write("You have not got in your pocket 100 & 1000");
 
 } else {
 
