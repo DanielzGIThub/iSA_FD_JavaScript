6101afd1d7164119c5d9029b59bffd219602ccb2
DanielzGIThub <dzmsgbox@gmail.com>
2019-10-09 19:32:56 +0200

    Document object model (DOM) getting Elements

--- hello.js
+++ hello.js
@@ -1,21 +1,17 @@
-var myDate = new Date();
-console.log(myDate);
-document.write(myDate);
+console.log(document.getElementsByClassName("lorem"));
 
-var myBirthday = new Date(1977, 9, 11, 9, 30, 25); //moths are counted fomr 0 to 11
-document.write(myBirthday.getFullYear());
-document.write(" ");
-document.write(myBirthday.getMonth());
-document.write(" ");
-document.write(myBirthday.getDate());
-document.write(" ");
-document.write(myBirthday.getDay());
-document.write(" ");
-document.write(myBirthday.getHours());
-document.write(" ");
-document.write(myBirthday.getMinutes());
-document.write(" ");
-document.write(myBirthday.getTime());
-document.write(" ");
-document.write(myBirthday.getSeconds());
-document.write(" ");
+var ps = document.getElementsByClassName("lorem");
+
+console.log(ps);
+
+
+
+ps[1].innerHTML = "change";
+
+
+var myHeading = document.getElementById("678");
+
+
+myHeading.innerHTML = "Corrected Heading";
+
+
