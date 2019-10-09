cbb2faeb62f439a05500754b2ad2903dcce00a71
DanielzGIThub <dzmsgbox@gmail.com>
2019-10-09 11:59:33 +0200

    Contructor function tested

--- hello.js
+++ hello.js
@@ -1,34 +1,18 @@
-var myArray = new Array();
+var Flat = function(numberOfRooms, furnitures) {
 
-myArray[0] = 7;
-myArray[1] = "text";
-myArray[3] = true;
-
-console.log(myArray);
-console.log(myArray.length);
-console.log(typeof(myArray[2]));
-
-var flat = new Object();
-
-flat.numberOfRooms = 4;
-flat.furnitures = "sofa";
-flat.location = function(){console.log("Pomeranian")};
-
-document.write(flat.numberOfRooms);
-document.write(flat.furnitures);
-flat.location();
-
-var flat2 = {
-
-    numberOfRooms: 5,
-    furnitures: "chairs",
-    location: function(code, city){console.log("Poland " + " " + code + " " + city)},
-    test: function(){console.log(this)},
-    infoOnLocation: function(){console.log(this.location())}
+    this.numberOfRooms = numberOfRooms;
+    this.furnitures = furnitures;
+    this.location = function(code, city){console.log("Poland " + " " + code + " " + city)};
+    this.test = function(){console.log(this)};
+    this.infoOnLocation = function(){console.log(this.location())};
 
 };
 
-console.log(flat2.furnitures);
-flat2.location("83-000", "Gdansk");
-flat2.test();
-flat2.infoOnLocation();
+var myFlat1 = new Flat(4, "sofa");
+var myFlat2 = new Flat(5, "chairs");
+var myFlat3 = new Flat(2, "table");
+
+
+myFlat2.location("83-000", "Pruszcz");
+myFlat2.infoOnLocation();
+console.log(myFlat2.furnitures);
