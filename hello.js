833cb31c91506ed93eb3feff56656a6f962936aa
DanielzGIThub <dzmsgbox@gmail.com>
2019-10-10 13:23:08 +0200

    Javascript event added, click, hover

--- hello.js
+++ hello.js
@@ -1,28 +1,9 @@
-var new_li = document.createElement("li");
-var new_li2 = document.createElement("li");
+var get_head = document.getElementById("678");
 
-var new_a = document.createElement("a");
-var new_a2 = document.createElement("a");
+get_head.onclick = function(){alert("You clicked the heading");};
 
-var get_ul = document.getElementsByTagName("ul");
+var get_p = document.getElementsByTagName("div")[0].getElementsByTagName("p")[4];
 
-console.log(new_li);
-console.log(new_a);
-console.log(get_ul[0]);
+console.log(get_p);
 
-get_ul[0].appendChild(new_li);
-
-new_li.appendChild(new_a);
-
-new_a.href = "https://github.com/DanielzGIThub";
-
-new_a.innerHTML = "shutterstock"
-
-get_ul[0].insertBefore(new_li2, get_ul[0].getElementsByTagName("li")[1]);
-
-new_li2.appendChild(new_a2);
-
-var removed_li = get_ul[0].removeChild(get_ul[0].getElementsByTagName("li")[4]);
-
-get_ul[0].appendChild(removed_li);
-
+get_p.onmouseover = function(){alert("you hovered me");};
