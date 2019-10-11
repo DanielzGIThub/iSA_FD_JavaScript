7e549835fade2b5543bcc5b2bf0d8a6bbb71fe25
DanielzGIThub <dzmsgbox@gmail.com>
2019-10-11 22:18:28 +0200

    window.onload event tested

--- hello.js
+++ hello.js
@@ -1,19 +1,29 @@
-var content = document.getElementById("content");
+function events(){
 
-console.log(content);
+    var content = document.getElementById("content");
 
-var button = document.getElementById("button");
+    console.log(content);
+
+    var button = document.getElementById("button");
 
 
 
-button.onclick = function(){
+    button.onclick = function(){
 
-    if(content.className == "open"){
-        content.className = "test";
-        button.innerHTML = "Show More";
-    } else if(content.className == "test"){
-        content.className = "open";
-        console.log(content);
-        button.innerHTML = "Show Less";
-    }
+        if(content.className == "open"){
+            content.className = "test";
+            button.innerHTML = "Show More";
+        } else if(content.className == "test"){
+            content.className = "open";
+            console.log(content);
+            button.innerHTML = "Show Less";
+        }
+    };
+
 };
+
+window.onload = function(){ //useful when .js is in <head>
+
+    events()
+
+}
